67edab833aecdff5ccc40d0d5f38f7fc1eb04542
SerjioNo <96014169+SerjioNo@users.noreply.github.com>
2022-06-07 17:59:31 +0300

    Delete src directory

--- src/js/task-05.js
+++
@@ -1,9 +0,0 @@
-
-
-const inputEl = document.querySelector("#name-input");
-const outputEl = document.querySelector("#name-output");
-
-inputEl.addEventListener("input", event => {
-inputEl.value.trim() === "" 
-? outputEl.textContent = "Anonymous"
-: outputEl.textContent = inputEl.value})
--- src/js/task-07.js
+++
@@ -1,9 +0,0 @@
-
-
-const inputRange = document.querySelector("#font-size-control");
-const textEl = document.querySelector("#text");
-
-const fontSize= inputRange.addEventListener('input', event => {
-    textEl.style.fontSize = event.currentTarget.value
-+ "px";
-});
